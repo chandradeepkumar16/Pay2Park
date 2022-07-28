c7e9fa00b2fc943ab115729fa92e1377918c9329
ayusshhhii <2as1931154@gmail.com>
2022-07-28 23:52:33 +0530

    made corrections

--- app/src/main/java/com/example/pay2park/RegisterParkingActivity.java
+++ app/src/main/java/com/example/pay2park/RegisterParkingActivity.java
@@ -1,60 +1,54 @@
 package com.example.pay2park;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
+import android.location.Address;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.NotNull;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 
 public class RegisterParkingActivity extends AppCompatActivity {
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        startActivity(new Intent(RegisterParkingActivity.this, UserAdminSelection.class));
-        finish();
-    }
-
     private EditText mlocality;
     private EditText mdetailadd;
-    private Button btninsert, viewdatabtn;
+    private Button btninsert , viewdatabtn;
+
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+//    DatabaseReference databaseReference;
+
+    DatabaseReference database_address;
+    // creating a variable for
+    // our object class
+    Address address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-//        getSupportActionBar().hide();
         setContentView(R.layout.activity_register_parking);
 
 
-        mlocality = findViewById(R.id.registerlocality);
-        mdetailadd = findViewById(R.id.registerparkingadd);
-        btninsert = findViewById(R.id.proceed_from_address_btn);
-        viewdatabtn = (Button) findViewById(R.id.viewdata_saved);
+        mlocality= findViewById(R.id.registerlocality);
+        mdetailadd= findViewById(R.id.registerparkingadd);
+        btninsert= findViewById(R.id.proceed_from_address_btn);
+        viewdatabtn=(Button) findViewById(R.id.viewdata_saved);
 
         // below line is used to get the
         // instance of our FIrebase database.
@@ -62,20 +56,19 @@ public class RegisterParkingActivity extends AppCompatActivity {
 //
 //        // below line is used to get reference for our database.
 //        databaseReference = firebaseDatabase.getReference("Address");
-
-
-        viewdatabtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(RegisterParkingActivity.this, Addresslist.class));
-                finish();
-            }
-        });
+        database_address=FirebaseDatabase.getInstance().getReference();
 
         btninsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegisterParkingActivity.this, DateSetActivity.class));
+                InsertData();
+            }
+        });
+
+        viewdatabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterParkingActivity.this , Addresslist.class));
                 finish();
             }
         });
@@ -108,7 +101,22 @@ public class RegisterParkingActivity extends AppCompatActivity {
 
     }
 
+    private void InsertData() {
+        String locality_add= mlocality.getText().toString().trim();
+        String address_full=mdetailadd.getText().toString().trim();
+        String id = database_address.push().getKey();
 
+        Addressdata addressdata = new Addressdata(locality_add , address_full);
+        database_address.child("Parking_address").child(id).setValue(addressdata)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterParkingActivity.this, "Address data inserted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
 //    private void addDatatoFirebase(String name, String detail) {
 //        // below 3 lines of code is used to set
