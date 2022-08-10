16fd09e2a85298eec32ece9b8cc648e51d480210
Chandradeep Kumar <chandradeepraj16.ck@gmail.com>
2022-08-10 17:28:28 +0530

    profile activity added

--- app/src/main/java/com/example/pay2park/ProfileActivity.java
+++ app/src/main/java/com/example/pay2park/ProfileActivity.java
@@ -1,14 +1,177 @@
 package com.example.pay2park;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private TextView p_num , p_name , p_dlno , p_vhtype , p_lastname , topname ,topemail;
+    private Button logout_btn;
+    private ImageView walletimg , settingimg;
+    FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth mAuth;
+    DatabaseReference dbref_pname , dbref_pnamel ,dbref_p_num , dbref_dlno , dbref_vhtype , dbref_email;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        p_num=findViewById(R.id.profile_num);
+        p_name=findViewById(R.id.profile_name);
+        p_dlno=findViewById(R.id.profile_dlno);
+        p_vhtype=findViewById(R.id.profile_vehicletype);
+        p_lastname=findViewById(R.id.profile_lastname);
+
+        logout_btn=(Button)findViewById(R.id.logout);
+
+        walletimg=(ImageView)findViewById(R.id.img_wallet);
+        settingimg=(ImageView)findViewById(R.id.img_setting);
+
+        topname=findViewById(R.id.top_name);
+        topemail=findViewById(R.id.top_email);
+
+
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        mAuth=FirebaseAuth.getInstance();
+
+        dbref_email=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("email");
+        dbref_pname=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Details").child("firstname");
+        dbref_pnamel=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Details").child("lastname");
+        dbref_p_num=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Details").child("contact");
+        dbref_dlno=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Details").child("uploaddl");
+        dbref_vhtype=firebaseDatabase.getReference("Users").child(mAuth.getCurrentUser().getUid()).child("Details").child("vehicletype");
+
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Toast.makeText(ProfileActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ProfileActivity.this, LogInActivity.class));
+            }
+        });
+
+
+        dbref_email.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st = snapshot.getValue(String.class);
+                topemail.setText(st);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+
+        dbref_pname.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st=snapshot.getValue(String.class);
+                p_name.setText(st);
+                topname.setText(st);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        dbref_pnamel.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st=snapshot.getValue(String.class);
+                p_lastname.setText(st);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+
+        dbref_p_num.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st=snapshot.getValue(String.class);
+                p_num.setText(st);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        dbref_dlno.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st=snapshot.getValue(String.class);
+                p_dlno.setText(st);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        dbref_vhtype.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                String st=snapshot.getValue(String.class);
+                p_vhtype.setText(st);
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
+            }
+        });
+
+        walletimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ProfileActivity.this, "Wallet Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        settingimg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ProfileActivity.this, "Setting clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/pay2park/UserAdminFragment.java
+++ app/src/main/java/com/example/pay2park/UserAdminFragment.java
@@ -15,6 +15,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class UserAdminFragment extends Fragment implements View.OnClickListener {
 
@@ -28,7 +29,10 @@ public class UserAdminFragment extends Fragment implements View.OnClickListener 
     LinearLayout buyer;
     LinearLayout seller;
     private View view;
-    private RelativeLayout loginSignup, bookmarks, gold, profile;
+
+    FirebaseAuth mauth;
+
+    private RelativeLayout loginSignup, rent_mypark, avl_park, profile;
     private TextView your_orders, fav_orders, address_book, online_ordering_help, send_feedback, report_safety, rateus;
 
     @Override
@@ -51,6 +55,7 @@ public class UserAdminFragment extends Fragment implements View.OnClickListener 
                 startActivity(new Intent(getActivity(), GetStartedActivity.class));
             }
         });
+
         seller=(LinearLayout) view.findViewById(R.id.sellerlayout);
         seller.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,36 +63,45 @@ public class UserAdminFragment extends Fragment implements View.OnClickListener 
                 startActivity(new Intent(getActivity(), RegisterParkingActivity.class));
             }
         });
+
+        mauth=FirebaseAuth.getInstance();
         drawerLayout = (DrawerLayout) view.findViewById(R.id.drawerLayout);
         navigationView = (NavigationView) view.findViewById(R.id.navigationView);
 
         navigationBar = (ImageView) view.findViewById(R.id.navigationBar);
 
         loginSignup = (RelativeLayout) view.findViewById(R.id.relativeLayout2);
-        bookmarks = (RelativeLayout) view.findViewById(R.id.relativeLayout3);
-        gold = (RelativeLayout) view.findViewById(R.id.relativeLayout4);
+        rent_mypark = (RelativeLayout) view.findViewById(R.id.relativeLayout3);
+        avl_park = (RelativeLayout) view.findViewById(R.id.relativeLayout4);
         profile=(RelativeLayout)view.findViewById(R.id.profile);
 
-        your_orders = (TextView) view.findViewById(R.id.your_orders);
-        fav_orders = (TextView) view.findViewById(R.id.fav_orders);
-        address_book = (TextView) view.findViewById(R.id.address_book);
-        online_ordering_help = (TextView) view.findViewById(R.id.online_ordering_help);
-        send_feedback = (TextView) view.findViewById(R.id.send_feedback);
-        report_safety = (TextView) view.findViewById(R.id.report_safety);
-        rateus = (TextView) view.findViewById(R.id.rateus);
+//        your_orders = (TextView) view.findViewById(R.id.your_orders);
+//        fav_orders = (TextView) view.findViewById(R.id.fav_orders);
+//        address_book = (TextView) view.findViewById(R.id.address_book);
+//        online_ordering_help = (TextView) view.findViewById(R.id.online_ordering_help);
+
+//        send_feedback = (TextView) view.findViewById(R.id.send_feedback);
+//        report_safety = (TextView) view.findViewById(R.id.report_safety);
+//        rateus = (TextView) view.findViewById(R.id.rateus);
+
 
         navigationBar.setOnClickListener(this);
         loginSignup.setOnClickListener(this);
-        bookmarks.setOnClickListener(this);
-        gold.setOnClickListener(this);
-        your_orders.setOnClickListener(this);
-        fav_orders.setOnClickListener(this);
-        address_book .setOnClickListener(this);
-        send_feedback.setOnClickListener(this);
-        report_safety.setOnClickListener(this);
-        rateus.setOnClickListener(this);
+        rent_mypark.setOnClickListener(this);
+        avl_park.setOnClickListener(this);
+
+//        your_orders.setOnClickListener(this);
+//        fav_orders.setOnClickListener(this);
+//        address_book .setOnClickListener(this);
+
+//        send_feedback.setOnClickListener(this);
+//        report_safety.setOnClickListener(this);
+//        rateus.setOnClickListener(this);
+
         profile.setOnClickListener(this);
+
     }
+    //RegisterParkingActivity
 
 
     @Override
@@ -96,38 +110,25 @@ public class UserAdminFragment extends Fragment implements View.OnClickListener 
             case R.id.navigationBar:
                 drawerLayout.openDrawer(navigationView, true);
                 break;
-            case R.id.your_orders:
-                Toast.makeText(getContext(), "your_orders", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.fav_orders:
-                Toast.makeText(getContext(), "fav_orders", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.address_book:
-                Toast.makeText(getContext(), "address_book", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.online_ordering_help:
-                Toast.makeText(getContext(), "online_ordering_help", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.send_feedback:
-                Toast.makeText(getContext(), "send_feedback", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.report_safety:
-                Toast.makeText(getContext(), "report_safety", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.rateus :
-                Toast.makeText(getContext(), "rateus", Toast.LENGTH_SHORT).show();
-                break;
+
             case R.id.relativeLayout2 :
-                Toast.makeText(getContext(), "loginsignup", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getContext(), "logged out", Toast.LENGTH_SHORT).show();
+                mauth.signOut();
                 Intent intent = new Intent(getActivity(), LogInActivity.class);
                 startActivity(intent);
                 break;
             case R.id.relativeLayout3 :
-                Toast.makeText(getContext(), "bookmark", Toast.LENGTH_SHORT).show();
+                //renting
+                Intent intent3= new Intent(getActivity(), RegisterParkingActivity.class);
+                startActivity(intent3);
                 break;
+
             case R.id.relativeLayout4 :
-                Toast.makeText(getContext(), "gold", Toast.LENGTH_SHORT).show();
+                //available parking
+                Intent intent2= new Intent(getActivity(), Addresslist.class);
+                startActivity(intent2);
                 break;
+
             case R.id.profile :
                 Toast.makeText(getContext(), "your profile", Toast.LENGTH_SHORT).show();
                 Intent intent1= new Intent(getActivity(), ProfileActivity.class);
